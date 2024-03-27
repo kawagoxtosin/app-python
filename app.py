e7ff8c6d29a3d53f7a6c2f78e7c94d38976fdae8
kawagoxtosin <163124116+kawagoxtosin@users.noreply.github.com>
2024-03-27 08:38:36 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+
+print ("""MARTIN BURGER KING
+        """)
+
+print("1- cadastrar restaurante")
+print("2- listar restaurante")
+print("3- ativar restaurante")
+print("4- sair")
+
+opcao_escolhida = int(input("escolha uma opção: "))
+#print("você escolheu a opção", opcao_escolhida)
+#print(f"você escolheu a opção {opcao_escolhida}")
