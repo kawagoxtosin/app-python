765ab81d77fdbbd7d6b3b18ae106ffbe4279262f
kawagoxtosin <163124116+kawagoxtosin@users.noreply.github.com>
2024-04-09 10:10:50 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,52 @@
 import os
 
-print ("""MARTIN BURGER KING
-        """)
+restaurantes = ['ice te pego','la casa de pastel']
 
-print("1- cadastrar restaurante")
-print("2- listar restaurante")
-print("3- ativar restaurante")
-print("4- sair")
+def exibir_nome_do_programa():
+ print ("""MARTIN BURGER KING
+        """)
+def exibir_opcoes():
+ print("1- cadastrar restaurante")
+ print("2- listar restaurante")
+ print("3- ativar restaurante")
+ print("4- sair")
+
+def finaliza_app():
+     exibir_subtitulo('Finalizar App')
+
+def voltar_ao_menu_principal():
+     input('\n Digite a tecla "Enter" para voltar ao menu principal')
+     main()
+
 
 
 opcao_escolhida = int(input("escolha uma opção: "))
 #print("você escolheu a opção", opcao_escolhida)
 #print(f"você escolheu a opção {opcao_escolhida}")
 
-def finaliza_app():
-        os.system('cls')
-        print('Encerrando o programa\n')
+def exibir_subtitulo(texto):
+ os.system('clear')
+ print(texto)
+ print()
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-elif opcao_escolhida == 3:
-       print('ativar restaurantes')
-else:
-        finaliza_app()
+def ecolher_opcao():
+ 
+        if opcao_escolhida == 1:
+                print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+                print('Listar restaurantes')
+        elif opcao_escolhida == 3:
+                print('ativar restaurantes')
+        else:
+                finaliza_app()
+
+def main():
+     os.system('clear')
+     exibir_nome_do_programa()
+     exibir_opcoes()
+     ecolher_opcao()
+
+if __name__ == '__main__':
+     main()
+
+
