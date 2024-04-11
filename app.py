2a4e1522a7c32dd8f21f164a38296af53697b72d
kawagoxtosin <163124116+kawagoxtosin@users.noreply.github.com>
2024-04-11 08:23:28 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -18,11 +18,9 @@ def voltar_ao_menu_principal():
      input('\n Digite a tecla "Enter" para voltar ao menu principal')
      main()
 
-
-
-opcao_escolhida = int(input("escolha uma opção: "))
-#print("você escolheu a opção", opcao_escolhida)
-#print(f"você escolheu a opção {opcao_escolhida}")
+def opcao_invalida():
+     print('Opção invalida!\n')
+     voltar_ao_menu_principal()
 
 def exibir_subtitulo(texto):
  os.system('clear')
@@ -30,15 +28,22 @@ def exibir_subtitulo(texto):
  print()
 
 def ecolher_opcao():
- 
+ try:
+        opcao_escolhida = int(input("escolha uma opção: "))
+
         if opcao_escolhida == 1:
                 print('Cadastrar restaurante')
         elif opcao_escolhida == 2:
                 print('Listar restaurantes')
         elif opcao_escolhida == 3:
                 print('ativar restaurantes')
-        else:
+        elif opcao_escolhida == 4:
                 finaliza_app()
+        else:
+                opcao_invalida()
+
+ except:
+      opcao_invalida()
 
 def main():
      os.system('clear')
